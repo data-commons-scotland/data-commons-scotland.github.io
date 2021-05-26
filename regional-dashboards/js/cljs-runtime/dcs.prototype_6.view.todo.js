94e4b12323aff36347a97188516305b912e4a0ae
Ash <amc@copper.lan>
2021-05-26 09:20:44 +0100

    Redirect from now defunct regional-dashboards context to the new prototype-6 context

--- regional-dashboards/js/cljs-runtime/dcs.prototype_6.view.todo.js
+++
@@ -1,6 +0,0 @@
-goog.provide('dcs.prototype_6.view.todo');
-dcs.prototype_6.view.todo.root = (function dcs$prototype_6$view$todo$root(){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"TODO"], null)], null);
-});
-
-//# sourceMappingURL=dcs.prototype_6.view.todo.js.map
