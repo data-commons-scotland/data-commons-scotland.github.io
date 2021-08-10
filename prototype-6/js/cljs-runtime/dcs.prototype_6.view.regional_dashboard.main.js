6cccdf1f059ebc7b4252feef0297ebdaef236c7c
Ash <amc@zinc.lan>
2021-08-10 15:09:26 +0100

    Added 12 'curiousities' to the regional dashboard page (previous broken)

--- prototype-6/js/cljs-runtime/dcs.prototype_6.view.regional_dashboard.main.js
+++ prototype-6/js/cljs-runtime/dcs.prototype_6.view.regional_dashboard.main.js
@@ -1,12 +1,12 @@
 goog.provide('dcs.prototype_6.view.regional_dashboard.main');
 dcs.prototype_6.view.regional_dashboard.main.root = (function dcs$prototype_6$view$regional_dashboard$main$root(route){
-var region = (function (){var G__33984 = route;
-var G__33984__$1 = (((G__33984 == null))?null:new cljs.core.Keyword(null,"parameters","parameters",-1229919748).cljs$core$IFn$_invoke$arity$1(G__33984));
-var G__33984__$2 = (((G__33984__$1 == null))?null:new cljs.core.Keyword(null,"query","query",-1288509510).cljs$core$IFn$_invoke$arity$1(G__33984__$1));
-if((G__33984__$2 == null)){
+var region = (function (){var G__33994 = route;
+var G__33994__$1 = (((G__33994 == null))?null:new cljs.core.Keyword(null,"parameters","parameters",-1229919748).cljs$core$IFn$_invoke$arity$1(G__33994));
+var G__33994__$2 = (((G__33994__$1 == null))?null:new cljs.core.Keyword(null,"query","query",-1288509510).cljs$core$IFn$_invoke$arity$1(G__33994__$1));
+if((G__33994__$2 == null)){
 return null;
 } else {
-return new cljs.core.Keyword(null,"region","region",270415120).cljs$core$IFn$_invoke$arity$1(G__33984__$2);
+return new cljs.core.Keyword(null,"region","region",270415120).cljs$core$IFn$_invoke$arity$1(G__33994__$2);
 }
 })();
 cljs.core.reset_BANG_(dcs.prototype_6.state.region_cursor,region);
